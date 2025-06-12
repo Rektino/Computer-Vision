6a7c751dbb3368889946254b8a33756bebdbf593
Giorgos Derlers <geoder.097@gmail.com>
2025-06-12 16:04:38 +0200

    First commit, basic CMake files and folder structure

---
+++ build/CMakeFiles/StereoVisionProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StereoVisionProject.dir/src/main.cpp.o"
+  "CMakeFiles/StereoVisionProject.dir/src/main.cpp.o.d"
+  "StereoVisionProject"
+  "StereoVisionProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StereoVisionProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
